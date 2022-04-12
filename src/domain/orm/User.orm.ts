bccbff09aa509ba86ac8c196c3062fb395627a0e
AyrtonRada <axel582011@hotmail.com>
2022-04-12 18:40:02 -0300

    configuracion para Mongodb

---
+++ src/domain/orm/User.orm.ts
@@ -0,0 +1,11 @@
+import { userEntity } from "../entities/User.entity";
+import { LogSuccess, LogError } from "../../utils/logger";
+
+export const GetAllUsers = async (): Promise<any[] | undefined> => {
+  try {
+    let userModel = userEntity();
+    return await userModel.find({ isDelete: false });
+  } catch (error) {
+    LogError(`[ORM ERROR]: Getting All Users: ${error}`);
+  }
+};
